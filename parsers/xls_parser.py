da7b9c3ac65fd18c57b344555c76f2148319a93a
wwydmanski <wwydmanski@gmail.com>
2021-01-09 15:26:40 +0100

    feat: covid tests' results parsing

--- parsers/xls_parser.py
+++ parsers/xls_parser.py
@@ -31,20 +31,42 @@ class XLSParser:
 
     @staticmethod
     def parse(fname):
+        def _parse_covid_res(filtered):
+            try:
+                result = filtered[filtered['Wynik']=='Wynik badania']
+                answer = result['Wartość'].values[0]
+            except IndexError as e:
+                result = filtered[filtered['Wynik']=='Wykrywanie materiału genetyczneg']
+                answer = result['Wartość'].values[0]
+                if "nie wykryto" in answer.lower():
+                    answer = 'ujemny'
+                elif "wykryto" in answer.lower():
+                    answer = 'dodatni'
+                elif "niejednoznaczny" in answer.lower():
+                    answer = 'niejednoznaczny'
+                else:
+                    print(answer)
+                    raise e
+
+            
+            return answer, result
+
         data = pd.read_html(fname)[0]
         name = data.columns[0][0]
         data.columns = data.columns.droplevel().droplevel()
         data = data.dropna()
         
         covid_test = data[data['Profil'] == 'Koronawirus SARS-CoV-2']
-        unique_dates = covid_test['Data wyk.'].unique()
+        unique_orders = covid_test['Nr zlecenia'].unique()
 
         # # %%
         covid_results = defaultdict(list)
-        for date in unique_dates:
-            filtered = covid_test[covid_test['Data wyk.']==date]
-            result = filtered[filtered['Wynik']=='Wynik badania']
-            answer = result['Wartość'].values[0]
+        for order in unique_orders:
+            filtered = data[data['Nr zlecenia']==order]
+            answer, result = _parse_covid_res(filtered)
+            order_date = result['Data zlec.'].values[0]
+            exec_date = result['Data wyk.'].values[0]
+            
             if answer == 'dodatni':
                 res = 1
             elif answer == 'ujemny':
@@ -52,20 +74,27 @@ class XLSParser:
             else:
                 res = -1
 
-            covid_results['Profil'].append('Koronawirus SARS-CoV-2')
-            covid_results['Test'].append('SARS-CoV-2')
-            covid_results['Data zlec.'].append(date)
-            covid_results['Data wyk.'].append(date)
-            covid_results['Nr zlecenia'].append(result['Nr zlecenia'].values[0])
-            covid_results['Wynik'].append(answer)
+            try:
+                additional = filtered[filtered['Wynik']=='Materiał diagnostyczny']
+                material = additional['Wartość'].values[0]
+            except IndexError as e:
+                material = '-'
+
+            covid_results['Profil'].append('SARS-CoV-2')
+            covid_results['Test'].append(answer)
+            covid_results['Data zlec.'].append(order_date)
+            covid_results['Data wyk.'].append(exec_date)
+            covid_results['Nr zlecenia'].append(order)
+            covid_results['Wynik'].append(material)
             covid_results['Wartość'].append(res)
             covid_results['Norma'].append('0 - 0')
             covid_results['Jedn.'].append('Obecność')
 
-        data = data.append(pd.DataFrame.from_dict(covid_results, orient='columns'))
+        data = data[data["Nr zlecenia"].apply(lambda x: x not in unique_orders)]
+        data = data.append(pd.DataFrame.from_dict(covid_results, orient='columns'), ignore_index=True).reset_index(drop=True)
         return data, name
 
 # %%
 if __name__ == "__main__":
-    data = XLSParser.parse("data/pacjent nr 1 excel.xls")
+    data, _ = XLSParser.parse("data/pacjent nr 10.xls")
     data
